d22b8918d7710365f4c01a9187fcc728e3aeb87d
Maria Nakhoul <31465978+marianakhoul@users.noreply.github.com>
2022-07-27 19:58:52 -0400

    Update buildGermlineSVPoN.R

--- code/buildGermlineSVPoN.R
+++ code/buildGermlineSVPoN.R
@@ -78,6 +78,7 @@ for (i in 1:length(sampleList)){
 	# load and combine SVABA SV results
 	suppressWarnings(svSample <- loadVCFtoDataTableByChromosome(normSVABAFiles[sampleList[i]], 
 			chr=chrs, genomeStyle=genomeStyle, applyFilter = FALSE))
+	message("HERE")
 	sv.SVABA <- rbind(sv.SVABA, cbind(Sample = id, svSample))
 }
 save.image(outImage)
